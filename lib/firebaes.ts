50442ef1c0cfbbd1726b576388b1c722180ebb57
a96363877 <a96363877@gmail.com>
2025-01-21 19:13:38 -0800

    ddd

--- lib/firebaes.ts
+++ lib/firebaes.ts
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: "AIzaSyDIUeFAbjrL265u-8WjQKC17QKUs530BKg",
+    authDomain: "kw-new-59cb9.firebaseapp.com",
+    projectId: "kw-new-59cb9",
+    storageBucket: "kw-new-59cb9.firebasestorage.app",
+    messagingSenderId: "1004892216777",
+    appId: "1:1004892216777:web:a86f1e237001b5a0a15c18",
+    measurementId: "G-5PWR9E9PV8"
 }
 
 const app = initializeApp(firebaseConfig)
